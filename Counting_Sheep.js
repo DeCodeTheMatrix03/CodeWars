fe7cea24d74cc4eb117d658560346143e1040799
Joel Nunez <90292480+DeCodeTheMatrix03@users.noreply.github.com>
2022-05-12 07:56:38 -0400

    Create Counting_Sheep.js

---
+++ Counting_Sheep.js
@@ -0,0 +1,5 @@
+//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+function countSheeps(arrayOfSheeps) {
+  return arrayOfSheeps.filter(Boolean).length;
+}
