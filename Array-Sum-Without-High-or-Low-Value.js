13e8a7da49092c9a6db0f96b173f69086bc67f1a
DeCodeTheMatrix03 <90292480+DeCodeTheMatrix03@users.noreply.github.com>
2022-06-30 18:06:39 -0400

    updated Codewars challenge

--- Array-Sum-Without-High-or-Low-Value.js
+++ Array-Sum-Without-High-or-Low-Value.js
@@ -1,5 +1,5 @@
 //Task Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value. Mind the input validation.
-
+// teehee
 //solution:
 
 function sumArray(array) {
