0166528e06e69f8c2bc7246598bdb6bcbf24b67d
Joel Nunez <90292480+DeCodeTheMatrix03@users.noreply.github.com>
2022-10-17 13:49:32 -0400

    Create Find_Capital_Letters.js

---
+++ Find_Capital_Letters.js
@@ -0,0 +1,16 @@
+/*
+Instructions
+
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example
+
+Test.assertSimilar( capitals('CodEWaRs'), [0,3,4,6] );
+*/
+function (word) {
+  var caps = [];
+  for(var i = 0; i < word.length; i++) {
+    if(word[i].toUpperCase() == word[i]) caps.push(i);
+  }
+  return caps;
+};
